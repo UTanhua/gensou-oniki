f37ff1e00a693ffd16228147fc596681bc4b270e
wb <710537031@qq.com>
2018-08-16 17:17:05 +0800

    chartinfo_database

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'song',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
