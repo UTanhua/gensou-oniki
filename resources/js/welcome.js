b984d84e962f138518a81a03c4d31b3c26a993fe
wb <710537031@qq.com>
2018-08-16 11:11:19 +0800

    homepage

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -1,17 +1,26 @@
 $(document).ready();
 
 $("#btn_1").mouseenter(function () {
-    $("#btn_1").css("opacity","0.86");
+    $("#btn_1").css("opacity","0.82");
+});
+$("#btn_1").mouseleave(function () {
+    $("#btn_1").css("opacity","1");
 });
 
 $("#btn_2").mouseenter(function () {
-    $("#btn_2").css("opacity","0.86");
+    $("#btn_2").css("opacity","0.82");
+});
+$("#btn_2").mouseleave(function () {
+    $("#btn_2").css("opacity","1");
 });
 
-$("#btn_1").onclick = function () {
+var btn_1 = document.getElementById("btn_1");
+var btn_2 = document.getElementById("btn_2");
+
+btn_1.onclick = function () {
 
 };
 
-$("#btn_2").onclick = function () {
+btn_2.onclick = function () {
     window.location.href = "http://www.sdvx.in/chunithm.html";
 };
