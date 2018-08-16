830e84ae55317aacfd36a98a630eefafb0e92538
wb <710537031@qq.com>
2018-08-16 10:58:12 +0800

    homepage

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -1,7 +1,11 @@
 $(document).ready();
 
-$(".btn").mouseenter(function () {
-    this.css("opacity","0.86");
+$("#btn_1").mouseenter(function () {
+    $("#btn_1").css("opacity","0.86");
+});
+
+$("#btn_2").mouseenter(function () {
+    $("#btn_2").css("opacity","0.86");
 });
 
 $("#btn_1").onclick = function () {
