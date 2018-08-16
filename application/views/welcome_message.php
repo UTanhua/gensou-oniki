910bbcfaf92c193c114de255ca3d095f794208a3
wb <710537031@qq.com>
2018-08-16 10:46:59 +0800

    homepage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Gensou-Oniki</title>
+    <base href="<?=base_url();?>" />
     <link rel="stylesheet" href="<?php echo base_url("resources/css/welcome.css")?>" />
+    <script type="text/javascript" src="<?php echo base_url("resources/js/jquery-1.11.0.js")?>"></script>
 </head>
 <body>
 
@@ -13,7 +15,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="bgImg">
         <img src="<?php echo base_url("resources/img/bg.jpg")?>"/>
     </div>
+    <div class="contain">
+        <p>Welcome to Qunithm</p>
+        <div class="btn" id="btn_1">谱面资料(持续更新)</div>
+        <div class="btn" id="btn_2">谱面保管所</div>
+    </div>
 </div>
 
+<script type="text/javascript" src="<?php echo base_url("resource/js/welcome.js")?>"></script>
 </body>
 </html>
---
+++ resources/js/welcome.js
@@ -0,0 +1,11 @@
+$(".btn").mouseenter(function () {
+    this.css("opacity","0.86");
+});
+
+$("#btn_1").onclick = function () {
+
+};
+
+$("#btn_2").onclick = function () {
+    window.location.href = "http://www.sdvx.in/chunithm.html";
+};
